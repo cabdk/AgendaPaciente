c6ec4e8142198661d0c2c2fed347ea7317813509
cabdk <c.bravo4@gmail.com>
2019-02-12 10:13:46 -0300

    version 1

---
+++ src/main/java/com/bolsasdeideas/springboot/app/AgendaPacientesApplication.java
@@ -0,0 +1,14 @@
+package com.bolsasdeideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaPacientesApplication.class, args);
+	}
+
+}
+
